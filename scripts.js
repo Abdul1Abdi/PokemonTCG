a3f3bfb75f1a15167c8fa5aca06d0882939357ed
Abdul1Abdi <83085930+Abdul1Abdi@users.noreply.github.com>
2021-10-02 15:48:43 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
 const app = {};
 
-app.key = "fe0f56e3-aec3-465e-a8b9-2fe96f277d60";
+app.search=function(){
+    $("#cardSearch").on("keyup",function(){
+        let value = $(this).val().toLowerCase();
+        console.log(value)
+        $(".cardList li").filter(function(){
+            $(this).toggle($(this).attr("class").toLowerCase().indexOf(value) > -1)
+        })
+        
+    })
+}
 
+app.updateCardList = function(cards){
+    $cardList.empty()
+    $(".cardListTitle").empty()
+    $("#cardSearch").remove()
+    $(".cardSection").prepend("<div class='flexContainer'><input type='text' id='cardSearch' placeholder='Search for a card...' ></div>")
+    $(".cardSection").prepend("<h2 class='cardListTitle'>Card List</h2>")
+    cards.forEach(function(card){
+        cardListHTML = `
+                        <li class="${card.name}">
+                            <a href="${card.tcgplayer.url}">
+                                <img src="${card.images.small}"  alt= "image of the ${card.name} set">
+                                <p>Price: $${card.tcgplayer.price.toFixed(2)}</p>
+                             <a>
+                        </li>
+                        `
+        $cardList.append(cardListHTML)
+    })
+}
 
 app.init = function (){
     $setList = $(".setList")
@@ -11,7 +38,6 @@ app.init = function (){
         url: 'https://api.pokemontcg.io/v2/sets',
         method: 'GET',
         dataType: 'json',
-        headers:{"X-Api-Key": app.key},
         data:{
             // q:`series:"Sword & Shield"`
             
@@ -35,8 +61,7 @@ app.init = function (){
     $setList.on("click",".setImage", function(e){
         e.preventDefault();
 
-        $cardList.empty()
-        $(".cardListTitle").empty()
+     
         const set = $(this).attr("id")
         $.ajax({
             url: 'https://api.pokemontcg.io/v2/cards',
@@ -48,7 +73,7 @@ app.init = function (){
                 
             }
         }).then(function(res){
-            $(".cardSection").prepend("<h2 class='cardListTitle'>Card List</h2>")
+            
             console.log(res)
            res.data.forEach(function(card){
                // Cards come in different types such as normal, reverseholofoil, holofoil, and the API has differently named nested object. We use a for loop to go through each nested object under prices and get their market price property and use the highest one
@@ -72,23 +97,14 @@ app.init = function (){
            res.data.sort((a,b) => b.tcgplayer.price - a.tcgplayer.price);
         //    console.log(res.data)  
 
-           res.data.forEach(function(card){
-                cardListHTML = `
-                                <li> 
-                                    <img src="${card.images.small}" alt= "image of the ${card.name} set">
-                                    <p>Price: $${card.tcgplayer.price.toFixed(2)}</p>
-                                </li>
-                                `
-                $cardList.append(cardListHTML)
-            console.log(card)
-            //  console.log(card.tcgplayer.price)
-
+           app.updateCardList(res.data)
+           app.search()
            })
            document.getElementById("cardSection").scrollIntoView();
         })
-    })
+    }
     
-}
+
 
 
 $(function(){
